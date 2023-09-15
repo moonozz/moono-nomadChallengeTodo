e3ec8a8dbbf3b7c22e11179efddd83be1bdbdc47
moonozz <http0127@naver.com>
2023-09-16 01:13:45 +0900

    :zap: Feat: 로컬에 done 처리 수정

--- src/todo.js
+++ src/todo.js
@@ -4,17 +4,15 @@ const todoInput = document.querySelector("#todo-form input");
 const todoInputBtn = document.querySelector("#todo-form button");
 
 const todo = document.querySelector(".todo-li");
-const todoText = document.querySelector(".todo-li p");
-const todoCheckBtn = document.querySelector(".todo-li button");
+// const todoText = document.querySelector(".todo-li p");
+const todoCheckBtn = document.querySelector(".todo-li div button");
 
-// todo input에 적은거 submit 하기
-const handleTodoSubmit = (e) => {
-  e.preventDefault();
+const TODOS_KEY = "todos";
 
-  const newTodo = todoInput.value;
-  todoInput.value = "";
-  // console.log(newTodo);
-  paintTodo(newTodo);
+let todos = [];
+
+const localTodos = () => {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 };
 
 // todo 등록 시간 노출 함수
@@ -23,23 +21,42 @@ const paintTime = () => {
 
   const hours = String(time.getHours()).padStart(2, "0");
   const min = String(time.getMinutes()).padStart(2, "0");
+  const sec = String(time.getSeconds()).padStart(2, "0");
 
-  return `${hours}:${min}`;
+  return `${hours}:${min}:${sec}`;
 };
 
 // todo li 생성하기
-const paintTodo = (newTodo) => {
+const paintTodo = (todoObj) => {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const div = document.createElement("div");
   const p = document.createElement("p");
   const button = document.createElement("button");
 
-  span.innerText = paintTime();
-  div.classList.add("todo-li");
-  button.classList.add("todo-check");
+  const parsedTodos = JSON.parse(getTodos);
+
+  // 시간 불러오기
+  if (parsedTodos && parsedTodos.length === todos.length) {
+    span.innerText = todoObj.time;
+  } else if (!parsedTodos || parsedTodos.length !== todos.length) {
+    span.innerText = paintTime();
+  }
+
+  li.classList.add("todo-li");
+  li.id = todoObj.id;
+
+  // check true, false 불러오기
+  if (parsedTodos && todoObj.done === false) {
+    console.log(todoObj.done);
+    li.classList.remove("done");
+  } else if (parsedTodos && todoObj.done === true) {
+    console.log(todoObj.done);
+    li.classList.add("done");
+  }
+
   button.addEventListener("click", doneTodo);
-  p.innerText = newTodo;
+  p.innerText = todoObj.text;
 
   li.append(span, div);
   div.append(p, button);
@@ -50,19 +67,62 @@ const paintTodo = (newTodo) => {
 // todo done 관리하기
 const doneTodo = (e) => {
   e.preventDefault();
-  const txt = e.target.parentElement.children[0];
 
-  if (e.target.className === "todo-check") {
-    // todoText.className = "done";
-    e.target.classList.add("todo-checked");
-    e.target.classList.remove("todo-check");
-    txt.className = "done";
-  } else if (e.target.className === "todo-checked") {
-    e.target.classList.add("todo-check");
-    e.target.classList.remove("todo-checked");
-    txt.classList.remove("done");
+  const getTodos = localStorage.getItem(TODOS_KEY);
+  const todoLi = e.target.parentElement.parentElement;
+  const parsedTodos = JSON.parse(getTodos);
+
+  const findId = () => {
+    parsedTodos.forEach((el) => {
+      if (parseInt(todoLi.id) === el.id) {
+        el.done = !el.done;
+      }
+    });
+    console.log(parsedTodos);
+    localTodos();
+  };
+
+  if (todoLi.classList.value === "todo-li") {
+    todoLi.classList.add("done");
+    findId();
+    todos = parsedTodos;
+
+    console.log(parsedTodos);
+  } else if (todoLi.classList.value === "todo-li done") {
+    todoLi.classList.remove("done");
+    findId();
+    todos = parsedTodos;
   }
+  localTodos();
 };
+
+// todo input에 적은거 submit 하기
+const handleTodoSubmit = (e) => {
+  e.preventDefault();
+
+  const newTodo = todoInput.value;
+  todoInput.value = "";
+  const todoObj = {
+    text: newTodo,
+    time: paintTime(),
+    done: false,
+    id: Date.now(),
+  };
+  paintTodo(todoObj);
+  todos.push(todoObj);
+  localTodos();
+};
+
+const getTodos = localStorage.getItem(TODOS_KEY);
+
+if (getTodos) {
+  const parsedTodos = JSON.parse(getTodos);
+  todos = parsedTodos;
+  parsedTodos.forEach((el) => {
+    console.log(el);
+    paintTodo(el);
+  });
+}
 
 todoForm.addEventListener("submit", handleTodoSubmit);
 todoCheckBtn.addEventListener("click", doneTodo);
