41e35640507b6278d4247fae068fb78144b0403a
moonozz <http0127@naver.com>
2023-09-15 18:18:15 +0900

    :zap: Feat: 유저 이름 로컬에 저장하고 불러오기

--- src/login.js
+++ src/login.js
@@ -5,18 +5,36 @@ const nameInput = document.querySelector("#name-form input");
 const profileFirstTxt = document.querySelector(".profile div");
 const profileName = document.querySelector(".name");
 
+const NAME_KEY = "name";
+const getUserName = localStorage.getItem(NAME_KEY);
+
+// username 입력하고 로컬에 저장하기
 const onLoginSubmit = (e) => {
   e.preventDefault();
 
   const userName = nameInput.value;
-  // console.log(nameInput.value);
-  if (userName) {
-    const userNameSplit = userName.split("");
 
+  if (userName) {
     loginModal.classList.add("none");
-    profileFirstTxt.innerText = userNameSplit[0];
-    profileName.innerText = userName;
+    paintUserName(userName);
+
+    localStorage.setItem(NAME_KEY, userName);
   }
 };
 
-nameForm.addEventListener("submit", onLoginSubmit);
+// 반복되는 항복 함수로 만들어주기
+const paintUserName = (username) => {
+  const userNameSplit = username.split("");
+
+  profileFirstTxt.innerText = userNameSplit[0];
+  profileName.innerText = username;
+};
+
+// 로컬에 name이 저장되어있다면 가져오기
+if (getUserName === null) {
+  loginModal.classList.remove("none");
+  nameForm.addEventListener("submit", onLoginSubmit);
+} else {
+  loginModal.classList.add("none");
+  paintUserName(getUserName);
+}
